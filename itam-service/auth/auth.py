047a4e6b77b3d70e2cb913f4bed004481044eefd
Anushka <anushkamatta99@gmail.com>
2021-07-22 22:08:04 +0530

    added authentication

--- itam-service/auth/auth.py
+++ itam-service/auth/auth.py
@@ -1,21 +1,38 @@
 from flask import Blueprint
 from flask import request
 from flask_cors import cross_origin
+from ..__init__ import User
+from ..__init__ import db
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user, login_required
 
-auth = Blueprint('auth',__name__)
+auth = Blueprint('auth', __name__)
 
-@auth.route('/register', methods = ["GET","POST"])
+
+@auth.route('/register', methods=["GET", "POST"])
+@cross_origin()
 def register():
-    return
+    resp = request.json
+    newuser = User(email=resp['email'], first_name=resp['Firstname'], last_name=resp['Lastname'], username=resp['username'], organization=resp['Organisation'], password=generate_password_hash(resp['password'], method='sha256'))
+    db.session.add(newuser)
+    db.session.commit()
+    return "Registration Successful!", 200
+
 
 @auth.route('/login', methods=["GET", "POST"])
 @cross_origin()
 def login():
-    username = request.json['username']
-    if (username == "hello"):
-        return "login success", 200
-    else:
-        return "login failed", 200
+    resp = request.json
+    user = User.query.filter_by(username=resp['username']).first()
+    if not user or not check_password_hash(user.password, resp['password']):
+        return "Enter valid username or password", 200
+    login_user(user, remember=resp['remember'])
+    return "Login Successful", 200
 
-
-
+@auth.route('/logout',methods = ["GET", "POST"])
+@cross_origin()
+@login_required
+def logout():
+    logout_user()
+    return "Logout Succesful!", 200
