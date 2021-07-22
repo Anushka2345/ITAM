057e5205883f74d9a7c82c84021b51cc1cb9b687
unicorn-io <tayalvishesh83@gmail.com>
2021-07-22 13:44:25 +0530

    Added static db

--- itam-service/__init__.py
+++ itam-service/__init__.py
@@ -2,24 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-
+    first_name = db.Column(db.String(20), nullable = False)
+    last_name = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable = False)
+    organization = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
-
+    def __repr__(self):
+        return "{}, {}, {}".format(self.username, self.email, self.password)
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
+    db.__init__(app)
+
     cors = CORS(app)
-
-    with app.app_context():
-     from .auth import auth
-    app.register_blueprint(auth.auth)
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     return app
